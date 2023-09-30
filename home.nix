57bfdeaf64dd73b6134d8194ed9641d96c0df7b0
Mohammed Naser <mnaser@vexxhost.com>
2023-09-29 22:42:40 -0400

    initial commit

---
+++ home.nix
@@ -0,0 +1,95 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "mnaser";
+  home.homeDirectory = "/Users/mnaser";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+
+    userName = "Mohammed Naser";
+    userEmail = "mnaser@vexxhost.com";
+
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+
+      pull = {
+        rebase = true;
+      };
+
+      push = {
+        autoSetupRemote = true;
+      };
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "fzf"
+        "git"
+        "kubectl"
+        "starship"
+      ];
+    };
+  };
+
+  programs.eza = {
+    enable = true;
+    enableAliases = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.gh = {
+    enable = true;
+  };
+
+  home.packages = with pkgs; [
+    krew
+    kubectl
+    nixpkgs-fmt
+    starship
+  ];
+}
