29742629ab00cec5d4f25e84c6483385512bcca1
Mohammed Naser <mnaser@vexxhost.com>
2024-01-31 15:51:51 -0500

    finally.

--- overlays/black.nix
+++ overlays/black.nix
@@ -1,15 +1,11 @@
-final: prev:
-# Within the overlay we use a recursive set, though I think we can use `final` as well.
-rec {
-  # nix-shell -p python.pkgs.my_stuff
-  python = prev.python.override {
-    # Careful, we're using a different final and prev here!
-    packageOverrides = final: prev: {
-      black = prev.black.overrideAttrs (attrs: {
-        version = "24.1.1";
-      });
-    };
-  };
-  # nix-shell -p pythonPackages.my_stuff
-  pythonPackages = python.pkgs;
+final: prev: {
+  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+    (
+      python-final: python-prev: {
+        black = python-prev.black.overridePythonAttrs (oldAttrs: {
+          version = "24.1.2";
+        });
+      }
+    )
+  ];
 }
