044c8779302229b650e9dfffddc656f1a2cbd575
Mohammed Naser <mnaser@vexxhost.com>
2024-01-31 16:20:20 -0500

    right version, now?

--- overlays/black.nix
+++ overlays/black.nix
@@ -2,8 +2,13 @@ final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (
       python-final: python-prev: {
-        black = python-prev.black.overridePythonAttrs (oldAttrs: {
-          version = "24.1.2";
+        black = python-prev.black.overridePythonAttrs (oldAttrs: rec {
+          version = "24.1.1";
+
+          src = oldAttrs.src.override {
+            inherit version;
+            hash = "sha256-SLV2Dcv+XPl/1PuiOUZoHzqBUUxquKRbUNpnrI+8bHs";
+          };
         });
       }
     )
